daffb47aef3fdd04062c3b3a19f2f6b8456688b5
haijin yan <life0fun@gmail.com>
2014-11-30 09:59:06 -0800

    link list reverse dup etc

--- reverselist.py
+++ reverselist.py
@@ -5,6 +5,95 @@ class Node:
         self.val = x
         self.next = None
 
+
+# iterate with 3 ptr, pre, cur, next, move while next.
+def rlist(l):
+    if not l or not l.next:
+        return l
+
+    pre, cur, next = None, l, l.next
+    while next:
+        cur.next = pre
+        pre, cur, next = cur, next, next.next
+    return cur
+
+# recursion with head, and pass in last iteration of head's prev for head's next.
+def rlist(pre, head):
+    if not head or not head.next:
+        return head
+
+    hnext = head.next
+    head.next = pre
+    return rlist(head, hnext)
+
+# reverse list by inject to head
+# swap cur's next to head, sift down cur, repeat.
+# swap 3 ptrs, hd.next where injected, prev and next of swapped item(cur's next).
+def reverse-inject-head(root):
+    hd = new Node()
+    hd.next = root
+    if not root or not root.next:
+        return root
+
+    while root.next:
+        swap = root.next  # swap node is root's next.
+        swap_next = swap.next
+        # inject swap to be hd
+        swap.next = hd.next
+        hd.next = swap.next
+        # for next iteration, inj swap_next
+        root.next = swap_next
+
+    return hd.next
+
+# look at head cur. if head cur next is same, reset head cur next to next's next.
+def remove-dup(root):
+    if not root or not root.next:
+        return root
+
+    cur = root
+    while cur.next:
+        nxt = cur.next
+        if nxt.value != cur.value:
+            cur = cur.next
+        else:
+            cur.next = nxt.next
+
+    return root
+
+
+# while loop to skip head and its dup. if head has dup, skip it.
+# recur with prev, next. else, update prev's next to head. recur head, head.next.
+def remove-all-dup(prev, head):
+    if not head:
+        return head
+
+    cur = head
+    while cur.next and cur.next.val == cur.val:
+        cur = cur.next
+    
+    # if head has dup, recur with prev and next
+    if head != cur:
+        return remove-all-dup(prev, cur.next)
+    else:
+        # update prev's next here, and recur.
+        prev.next = head
+        return remove-all-dup(head, head.next)
+
+def remove-all-dup(head):
+    if not head or not head.next:
+        return head
+
+    cur = head
+    while cur.next and cur.next.val == head.val:
+        cur = cur.next
+
+    if cur != head:
+        return remove-all-dup(cur.next)
+    else:
+        head.next = remove-all-dup(head.next)
+        return head
+
 def reorderList(head):
     print "head ", head.val
     if not head.next or not head.next.next:
